8e16bcb4ed1b692b9e7b4f1b119ed44b819002a1
Peter Benke <pbenke@allplan.com>
2024-11-08 12:23:27 +0100

    Upgrade to TYPO3 v12: Update controller incl. Service.yaml

--- Classes/Controller/ConcertController.php
+++ Classes/Controller/ConcertController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace PeterBenke\PbConcertlist\Controller;
 
 /**
@@ -20,6 +22,7 @@ use TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException;
  */
 
 use Psr\Http\Message\ResponseInterface;
+use TYPO3Fluid\Fluid\View\ViewInterface;
 
 
 /**
